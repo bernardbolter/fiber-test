d438c36fe81e81dc7b73f919e4caa0fa9156041a
Besco <bernardbolter@gmail.com>
2023-11-29 20:25:01 +0100

    added dynamic

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,13 @@
 "use client"
 
+import dynamic from 'next/dynamic'
 import styles from './page.module.css'
 
-import MindThree from '@/components/mind-three'
+// import MindThree from '@/components/mind-three'
+const MindThree = dynamic(
+  () => import('../components/mind-three'),
+  { ssr: false}
+)
 
 const Home = () => {
   return (
